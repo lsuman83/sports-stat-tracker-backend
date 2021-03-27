cd73ab4398083730aa8c29430fe59e30124f3128
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-03-26 21:15:47 -0400

    modifying url for player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,11 @@
 class Player < ApplicationRecord
     has_one_attached :player_pic
-    
+
     belongs_to :team
     has_many :games
+
+    def player_pic_url
+        Rails.application.routes.url_helpers.url_for(player_pic) if player_pic.attached?
+    end
 
 end
